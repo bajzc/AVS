272aa10d6680d2a3e7a5bd7908f1e38685380b64
Zecheng Li <lzecheng98@gmail.com>
2025-08-13 10:21:52 +0100

    update run script

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -e
 rm -vf callgrind* cachegrind*
 
 cmake -B build .
-cmake --build build
+cmake --build build --config RelWithDebInfo -j 4
 
 valgrind --tool=callgrind --cache-sim=yes --dump-instr=yes --callgrind-out-file=callgrind.soa.out \
          --D1=4096,8,64 ./build/soa
